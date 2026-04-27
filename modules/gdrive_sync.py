d5855b6c3e72a151569d5cde8e5802d98ec6c26c
alaskowski314-eng <alaskowski3.14@gmail.com>
2026-04-27 22:49:50 +0200

    Fix formatting issues in gdrive_sync.py

--- modules/gdrive_sync.py
+++ modules/gdrive_sync.py
@@ -4,7 +4,7 @@ from google.oauth2.service_account import Credentials
 from googleapiclient.discovery import build
 from googleapiclient.http import MediaIoBaseUpload, MediaIoBaseDownload
 
-PARENT_FOLDER_ID = "1ylGjiavg_QQE2a0jIZW0TNOz_CibmIUN""
+PARENT_FOLDER_ID = "1ylGjiavg_QQE2a0jIZW0TNOz_CibmIUN"
 
 class CloudFile(io.BytesIO):
     """Klasa udająca plik wgrany przez Streamlit (ma atrybut .name)"""
